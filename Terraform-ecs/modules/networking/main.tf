a0f11491925a8f2e79f7767db9160be15b453b3d
zakariyaa4458 <zakariya.a4458@gmail.com>
2026-07-20 10:17:51 +0100

    changed the name of the Terraform folder and added paths to pipelines

--- Terraform-ecs/modules/networking/main.tf
+++ Terraform-ecs/modules/networking/main.tf
@@ -10,7 +10,7 @@ resource "aws_acm_certificate_validation" "cert_validation" {
 
 
   certificate_arn         = aws_acm_certificate.cert.arn
- # validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+ 
 
 }
 
